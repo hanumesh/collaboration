ba8f1428b6414c886bfb23c8663a0be7b20f8540
Hanumesh Palla <hanumesh.palla59@gmail.com>
2020-10-12 15:01:53 +0000

    automate ideas page

--- collaboration-ui/src/app/app-routing.module.ts
+++ collaboration-ui/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AutomateIdeaComponent } from './automate-idea/automate-idea.component';
+import { TrendsComponent } from './trends/trends.component';
+import { MyTasksComponent } from './my-tasks/my-tasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", pathMatch: "full",redirectTo: ""},
+  //{path: "home", component: HomeCoautomateIdeamponent},
+  {path: "automateIdea", component: AutomateIdeaComponent},
+  {path: "trends", component: TrendsComponent},
+  {path: "my-tasks", component: MyTasksComponent}  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- collaboration-ui/src/app/app.module.ts
+++ collaboration-ui/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MyTasksComponent } from './my-tasks/my-tasks.component';
+import { TrendsComponent } from './trends/trends.component';
+import { AutomateIdeaComponent } from './automate-idea/automate-idea.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MyTasksComponent,
+    TrendsComponent,
+    AutomateIdeaComponent
   ],
   imports: [
     BrowserModule,
